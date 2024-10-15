a3008840a1e4abb59b5a2c040526beb91a451da2
Amritendu Nath <amritendunath@yahoo.com>
2024-10-15 09:58:10 +0530

    c to g

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import ListHeader from "./Components/ListHeader";
 import { useEffect } from 'react';
+// import ListItem from "./Components/ListItem";
 
 const App = () => {
+  const userEmail = "anath@gmail.com"
+  // const [tasks, setTask] = useState(null);
 
   const getData =async()=>{
-    const userEmail = "anath@chegg.com"
     try {
       const response = await fetch(`http://localhost:8000/qtodos/${userEmail}`);
       const json = await response.json();
       console.log(json)
+      // setTask(json)
     } catch (error) {
       console.error(error)
     }
@@ -18,9 +21,15 @@ const App = () => {
     getData()
   },[])
 
+  // console.log(tasks)
+
+  //sort by date
+  // const sortedTasks = tasks?.sort((a,b)=>new Date(a.dueDate)-new Date(b.dueDate))
+
   return (
     <div className="app">
       <ListHeader listItem={"🏝️Holiday Tick List"}/>
+      {/* {sortedTasks?.map((task)=><ListItem key={task.id} task={task}/>)} */}
     </div>
   );
 }
