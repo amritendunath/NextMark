83562177bc36f6c8ebb5b351e5f9201e10cb2a1b
Amritendu Nath <amritendunath@yahoo.com>
2024-10-16 13:43:37 +0530

    Building Modal component

--- client/src/Components/ListHeader.js
+++ client/src/Components/ListHeader.js
@@ -1,15 +1,17 @@
-const ListHeader = ({listItem}) => {
+import Modal from "./Modal";
+const ListHeader = ({listName}) => {
 
     const signout = () => {
         console.log("signout");
     }
     return (
       <div className="list-header">
-        <h1>{listItem}</h1>
+        <h1>{listName}</h1>
         <div className="button-container">
             <button className="create">ADD NEW</button>
             <button className="signout" onClick={signout}>SIGN OUT</button>
         </div>
+        <Modal/>
       </div>
     );
   }
--- client/src/Components/Modal.js
+++ client/src/Components/Modal.js
@@ -1,10 +1,45 @@
-const Modal = () => {
-    return (
-      <div>
-        Anath
-      </div>
-    );
+import {useState} from 'react'
+
+const Modal = () =>{
+  const [data, setData]=useState(null)
+
+const mode = 'edit'
+  const handleChange = () => {
+    console.log('changing')
   }
-  
-  export default Modal;
-  
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <div className="form-title-container">
+          <h3>Let's {mode} you task</h3>
+          <button>X</button>
+        </div>
+        <form>
+          <input
+            required
+            maxLength={30}
+            placeholder="Your task goes here"
+            name ="title"
+            value={""}
+            onChange={handleChange}
+          />
+          <br/>
+          <label for="range">Drag to select your current progress</label>
+          <input
+            required
+            type="range"
+            id="range"
+            min="0"
+            max="100"
+            name="progress"
+            value={""}
+            onChange={handleChange}
+          />
+          <input className={mode} type="submit"/>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
