50c3f4f41a421d0ee127860db7d4e97d0df3e033
Amritendu Nath <amritendunath@yahoo.com>
2024-10-19 20:46:05 +0530

    setting onClick using hooks for the buttons

--- client/src/Components/Modal.js
+++ client/src/Components/Modal.js
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 
-const Modal = () =>{
-  const mode = 'create'
+const Modal = ({mode, setShowModal}) =>{
+  // const mode = 'create'
   const editMode = mode === 'edit' ? true : false;
   const [data, setData]=useState({
     user_email : "",
@@ -28,7 +28,7 @@ const Modal = () =>{
       <div className="modal">
         <div className="form-title-container">
           <h3>Let's {mode} your task</h3>
-          <button>X</button>
+          <button onClick={()=>setShowModal(false)}>X</button>
         </div>
         <form>
           <input
