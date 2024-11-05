d930cec1660a479348ef81f8492b240776effa44
Amritendu Nath <amritendunath@yahoo.com>
2024-11-05 08:44:38 +0530

    building auth component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import ListHeader from "./Components/ListHeader";
 import { useState, useEffect } from 'react';
 import ListItem from "./Components/ListItem";
+import Auth from "./Components/Auth";
 
 const App = () => {
   const userEmail = "anath@gmail.com"
   const [tasks, setTask] = useState(null);
 
-  const getData =async()=>{
+  const authToken = false;
+
+  const getData = async () => {
     try {
       const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`);
       const json = await response.json();
@@ -17,19 +20,24 @@ const App = () => {
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getData()
-  },[])
+  }, [])
 
   console.log(tasks)
 
   //sort by date
-  const sortedTasks = tasks?.sort((a,b)=> new Date(a.dueDate) - new Date(b.dueDate))
+  const sortedTasks = tasks?.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate))
 
   return (
     <div className="app">
-      <ListHeader listName={"🏝️Holiday Tick List"} getData={getData}/>
-      {sortedTasks?.map((task)=> <ListItem key={task.id} task={task} getData={getData}/>)}
+      {!authToken && <Auth />}
+      {authToken &&
+        <>
+          <ListHeader listName={"🏝️Holiday Tick List"} getData={getData} />
+          {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
+        </>
+      }
     </div>
   );
 }
--- client/src/Components/Auth.js
+++ client/src/Components/Auth.js
@@ -1,9 +1,41 @@
+import { useState } from 'react';
+
 const Auth = () => {
-    return (
-      <div>
-        Anath
-      </div>
-    );
+  const [error, setError] = useState(null)
+  const [isLogin, setIsLogin] = useState(true);
+
+  const viewLogin = (status) => {
+    setError(null)
+    setIsLogin(status)
   }
-  
-  export default Auth;
+
+  return (
+    <div clasName="auth-container">
+      <div clasName="auth-container">
+        <form>
+          <h2>{isLogin ? 'Please LogIn' : 'Please SignUp'}</h2>
+          <input type="email" placeholder="email"></input>
+          <input type="passsword" placeholder="password"></input>
+          {!isLogin && <input type="password" placeholder="confirm password"></input>}
+          <input type="submit" className="create"></input>
+          {error && <p>{error}</p>}
+        </form>
+        <div className="auth-options">
+          <button onClick={() => viewLogin(true)}
+            style={{ backgroundColor: isLogin ? 'rgb(255, 255, 255)' : 'rgb(188, 188, 188)' }}
+          >
+            Login
+          </button>
+          <button onClick={() => viewLogin(false)}
+            style={{ backgroundColor: !isLogin ? 'rgb(255, 255, 255)' : 'rgb(188, 188, 188)' }}
+          >
+            SignUp
+          </button>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Auth;
