5578b377d8fec5f19957795757eea7f0f3f05a83
Amritendu Nath <amritendunath@yahoo.com>
2024-11-06 11:49:41 +0530

    adding cookies in Modal for cookies.Email in data

--- client/src/Components/Modal.js
+++ client/src/Components/Modal.js
@@ -1,10 +1,14 @@
 import {useState} from 'react'
+import {useCookies} from 'react-cookie'
 
 const Modal = ({mode, setShowModal, getData, task}) =>{
+
+  const [cookies, setCookie, removeCookie] = useCookies(null)
   // const mode = 'create'
   const editMode = mode === 'edit' ? true : false;
+  
   const [data, setData]=useState({
-    user_email : editMode ? task.user_email : 'anath@gmail.com',
+    user_email : editMode ? task.user_email : cookies.Email,
     title: editMode ? task.title : "",
     progress: editMode ? task.progress : 50,
     date: editMode ? task.date : new Date()  
