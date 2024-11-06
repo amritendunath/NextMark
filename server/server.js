d24333bf10a7c25f34406281f04b174cada0901a
Amritendu Nath <amritendunath@yahoo.com>
2024-11-06 11:56:05 +0530

    created toekn func and sent via res.json({})

--- server/server.js
+++ server/server.js
@@ -82,7 +82,8 @@ app.post('/signup', async(req,res)=>{
   const hashedPassword = bcrypt.hashSync(password, salt)
   try {
     const signup = await signUp(email, hashedPassword)
-    res.json({email, password, signup})
+    const token = jwt.sign({email}, 'secret', {expiresIn: '1hr'})
+    res.json({email, token, signup})
   } catch (error) {
     console.log(error)
   }
