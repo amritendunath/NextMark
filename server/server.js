48068d8feb956a573e6e6b1d7189802d039e11db
Amritendu Nath <amritendunath@yahoo.com>
2024-11-09 22:30:59 +0530

    login route created in server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
+require('dotenv').config()
 const PORT = process.env.PORT ?? 8000
 const express = require('express')
 const app = express()
-const {v4: uuidv4} = require('uuid');
-const {fetchItems, queryItems, createItem, editItem, deleteItem, signUp} = require('./dynamoDB')
-const dotenv = require('dotenv')
+const { v4: uuidv4 } = require('uuid');
+const { fetchItems, queryItems, createItem, editItem, deleteItem, signUp, login } = require('./dynamoDB')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-
-app.use(cors())
 app.use(express.json())
-dotenv.config()
+
+app.use(cors({
+  origin:"*",
+}))
+// dotenv.config()
 
 // get all todos
 app.get('/todos', async (req, res) => {
@@ -26,7 +28,7 @@ app.get('/todos', async (req, res) => {
 
 // get queryTodos
 app.get('/todos/:userEmail', async (req, res) => {
-  const {userEmail} = req.params;
+  const { userEmail } = req.params;
   try {
     const todos = await queryItems(userEmail);
     console.log(todos)
@@ -36,11 +38,11 @@ app.get('/todos/:userEmail', async (req, res) => {
   }
 })
 //create a todo
-app.post('/todos', async(req,res)=>{
-  const {user_email, title, progress, date} =req.body;
+app.post('/todos', async (req, res) => {
+  const { user_email, title, progress, date } = req.body;
   const id = uuidv4();
   try {
-    const newTodo = await createItem(id, user_email, title, progress, date);
+    const newTodo = await createItem({ id, user_email, title, progress, date });
     console.log(newTodo)
     res.json(newTodo)
   } catch (error) {
@@ -53,8 +55,8 @@ app.get('/', (req, res) => {
 
 //edit Data
 app.put('/todos/:id', async (req, res) => {
-  const {id} = req.params;
-  const {user_email, title, progress, date} =req.body;
+  const { id } = req.params;
+  const { user_email, title, progress, date } = req.body;
   try {
     const editTodo = await editItem(id, user_email, title, progress, date);
     res.json(editTodo)
@@ -64,9 +66,9 @@ app.put('/todos/:id', async (req, res) => {
 })
 
 //delete Data
-app.delete('/todos/:id', async(req,res)=>{
-  const {id}=req.params;
-  const {user_email} = req.body
+app.delete('/todos/:id', async (req, res) => {
+  const { id } = req.params;
+  const { user_email } = req.body
   try {
     const deleteTodo = await deleteItem(id, user_email);
     res.json(deleteTodo)
@@ -76,14 +78,30 @@ app.delete('/todos/:id', async(req,res)=>{
 })
 
 //signup
-app.post('/signup', async(req,res)=>{
-  const {email, password}= req.body
+app.post('/signup', async (req, res) => {
+  const { email, password } = req.body
   const salt = bcrypt.genSaltSync(10)
   const hashedPassword = bcrypt.hashSync(password, salt)
   try {
     const signup = await signUp(email, hashedPassword)
-    const token = jwt.sign({email}, 'secret', {expiresIn: '1hr'})
-    res.json({email, token, signup})
+    const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' })
+    res.json({email, token})
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+//Login
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const user = await login(email)
+    const success = await bcrypt.compare(password, user.Items[0].hashedPassword)
+    const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' })
+    // if (success) {
+    //   res.json({ email: user.Items[0].email, token })
+    // }
+    return success ? res.json({ email: user.Items[0].email, token }) : res.status(400).json({ error: "Invalid Login" })
   } catch (error) {
     console.log(error)
   }
