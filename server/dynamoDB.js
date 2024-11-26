d19e7efc65158c4a5d0f4ee19643dd768158facf
Amritendu Nath <amritendunath@yahoo.com>
2024-11-26 07:03:26 +0530

    modifying lines in dynamoDB

--- server/dynamoDB.js
+++ server/dynamoDB.js
@@ -23,17 +23,17 @@ const fetchItems = async () => {
 };
 
 //to get desire items from todos
-const queryItems = async (userEmail) => {
-    // if (!userEmail) {
-    //     console.error('Invalid input: id and user_email must be provided');
-    //     return [];
-    // }
+const queryItems = async (user_email) => {
+    if (!user_email) {
+        console.error('Invalid input: id and user_email must be provided');
+        return [];
+    }
     const params = {
-        TableName: 'todos',// Your DynamoDB table name
+        TableName: 'todos',
         IndexName: 'user_email-index',
         KeyConditionExpression: 'user_email= :user_email',
         ExpressionAttributeValues: {
-            ':user_email': userEmail
+            ':user_email': user_email
         },
     };
 
@@ -51,7 +51,7 @@ const queryItems = async (userEmail) => {
 };
 
 //to create a todo (postData)
-const createItem = async ({id, user_email, title, progress, date}) => {
+const createItem = async (id, user_email, title, progress, date) => {
     const params = {
         TableName: 'todos',
         Item: {
@@ -65,22 +65,22 @@ const createItem = async ({id, user_email, title, progress, date}) => {
 
     try {
         const command = new PutCommand(params);
-        const response = await docClient.send(command);
+        const data = await docClient.send(command);
         console.log(response);
         console.log('Item added successfully');
-        return response
+        return data.Items
     } catch (error) {
         console.error('Unable to add item. Error:', JSON.stringify(error, null, 2));
     }
 };
 
 //to edit a todo (editData)
-const editItem = async (id, user_email, title, progress, date) => {
+const editItem = async ({noteId, user_email, title, progress, date}) => {
     // Implementation for editing an item
     const params = {
         TableName: 'todos',
         Key: {
-            id: id,
+            id: noteId,
             user_email: user_email
         },
         UpdateExpression: 'set #t = :title, #p = :progress, #d = :date',
@@ -98,21 +98,21 @@ const editItem = async (id, user_email, title, progress, date) => {
     };
     try {
         const command = new UpdateCommand(params);
-        const response = await docClient.send(command);
+        const data = await docClient.send(command);
         console.log(response);
         console.log('Item updated successfully');
-        return response;
+        return data;
     } catch (error) {
         console.error('Unable to update item. Error:', JSON.stringify(error, null, 2));
     }
 };
-
+ 
 //to delete a node
-const deleteItem = async (id, user_email) => {
+const deleteItem = async (noteId, user_email) => {
     const params = {
         TableName: 'todos',
         Key: {
-            id: id,
+            id: noteId,
             user_email: user_email
         },
     };
@@ -128,12 +128,12 @@ const deleteItem = async (id, user_email) => {
 }
 
 //to sign up
-const signUp = async (email, hashedPassword) => {
+const signUp = async (email, password) => {
     const params = {
         TableName: 'users',
         Item: {
             email: email,
-            hashedPassword: hashedPassword
+            hashedPassword: password
         }
     };
     try {
@@ -141,7 +141,7 @@ const signUp = async (email, hashedPassword) => {
         const response = await docClient.send(command);
         console.log(response);
         console.log('SignedUp successfully');
-        return response
+        return {email}
     } catch (error) {
         console.error('Unable to add item. Error:', JSON.stringify(error, null, 2));
     }
@@ -159,8 +159,8 @@ const login = async (email) => {
     try {
         const command = new QueryCommand(params);
         const data = await docClient.send(command);
-        console.log(command)
-        return data
+        console.log(data)
+        return data.Items[0]
     } catch (error) {
         console.error('Unable to query the table. Error:', JSON.stringify(error, null, 2));
         return [];
