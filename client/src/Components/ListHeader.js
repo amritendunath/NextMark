62fa2894d0c4cd506e1633942655e0f7199110d3
Amritendu Nath <amritendunath@yahoo.com>
2024-11-29 14:33:00 +0530

    saving the token from cookies to localstorage

--- client/src/Components/ListHeader.js
+++ client/src/Components/ListHeader.js
@@ -1,25 +1,30 @@
 import { useState } from "react";
 import Modal from "./Modal";
-import { useCookies } from "react-cookie";
-const ListHeader = ({listName, getData}) => {
-  const [cookies, setCookie, removeCookie] = useCookies(null);  
+import { useNavigate } from "react-router-dom";
+
+const ListHeader = ({ listName, getData }) => {
   const [showModal, setShowModal] = useState(null);
-    const signout = () => {
-        console.log("signout");
-        removeCookie("Email");
-        removeCookie("AuthToken");
-        window.location.reload();
-    }
-    return (
-      <div className="list-header">
-        <h1>{listName}</h1>
-        <div className="button-container">
-            <button className="create" onClick={()=>setShowModal(true)}>ADD NEW</button>
-            <button className="signout" onClick={signout}>SIGN OUT</button>
-        </div>
-        {showModal && <Modal mode={"create"} setShowModal={setShowModal} getData={getData}/>}
-      </div>
-    );
+
+  const navigate = useNavigate();
+  const signout = () => {
+    localStorage.clear()
+    navigate("/login");
   }
-  
-  export default ListHeader;
+//   useEffect(() => {
+//     console.log('useEffect triggered')
+//     getData();
+//     return () => {};
+//   },[]);
+  return (
+    <div className="list-header">
+      <h1>{listName}</h1>
+      <div className="button-container">
+        <button className="create" onClick={() => setShowModal(true)}>ADD NEW</button>
+        <button className="signout" onClick={signout}>SIGN OUT</button>
+      </div>
+      {showModal && <Modal mode={"create"} setShowModal={setShowModal} getData={getData} />}
+    </div>
+  );
+}
+
+export default ListHeader;
