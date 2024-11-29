575284a162309d39ae1c8f4661d75c7e6e34a98a
Amritendu Nath <amritendunath@yahoo.com>
2024-11-29 14:28:35 +0530

    Fetch ->-> Axios

--- client/src/Components/ListItem.js
+++ client/src/Components/ListItem.js
@@ -1,45 +1,38 @@
 import { useState } from "react";
-import TickIcon from "./TickIcon";  
+import TickIcon from "./TickIcon";
 import ProgressBar from "./ProgressBar";
 import Modal from "./Modal";
+import axiosInstance from "../utils/axiosInstance";
 
-const ListItem = ({task,getData}) => {
-  const [showModal, setShowModal] = useState(false);
-
-  const deleteData = async(e)=>{
-    e.preventDefault()
-    try {
-      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${task.id}`,{
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(task)
-      })
-      if(response.status === 200){
-        console.log('Task deleted')
-        getData()
-      }
-    } catch (error) {
-      console.log(error)
+const ListItem = ({ task, getData }) => {
+    const [showModal, setShowModal] = useState(false);
+    const deleteData = async () => {
+        const noteId = task.id
+        try {
+            const response = await axiosInstance.delete(`/delete-note/` + noteId)
+            if (response.data && !response.data.error) {
+                console.log("Note Deleted Successfully", "delete");
+                getData();
+            }
+        } catch (error) {
+            console.log(error)
+        }
     }
-  }
-
     return (
-      <li className="list-item">
-        <div className="info-container">
-          <TickIcon/>
-          <p className="task-title">{task.title}</p>
-          <ProgressBar/>
-        </div>
+        <li className="list-item">
+            <div className="info-container">
+                <TickIcon />
+                <p className="task-title">{task.title}</p>
+                <ProgressBar />
+            </div>
 
-        <div className="button-container">
-          <button className="edit" onClick={()=>setShowModal(true)}>Edit</button>
-          <button className="delete" onClick={deleteData}>Delete</button>
-        </div>
-        {showModal && <Modal mode={"edit"} setShowModal={setShowModal} task={task} getData={getData}/>}
-      </li>
+            <div className="button-container">
+                <button className="edit" onClick={() => setShowModal(true)}>Edit</button>
+                <button className="delete" onClick={deleteData}>Delete</button>
+            </div>
+            {showModal && <Modal mode={"edit"} setShowModal={setShowModal} task={task} getData={getData} />}
+        </li>
     );
-  }
-  
-  export default ListItem;
+}
+
+export default ListItem;
