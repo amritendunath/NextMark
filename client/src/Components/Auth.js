64b7aabfc263da9b2536b69551919cac93a036a6
Amritendu Nath <amritendunath@yahoo.com>
2024-11-29 14:22:57 +0530

    modified in Modal.js

--- client/src/Components/Auth.js
+++ client/src/Components/Auth.js
@@ -1,57 +1,67 @@
 import { useState } from 'react';
-import {useCookies} from 'react-cookie'
+import axiosInstance from '../utils/axiosInstance';
+import { useNavigate } from 'react-router-dom';
+import { validateEmail } from "../utils/helper";
 
-const Auth = () => {
-  const [cookies, setCookie, removeCookie] = useCookies(null)
+const Auth = ({getData}) => {
   const [error, setError] = useState(null)
   const [isLogin, setIsLogin] = useState(true);
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
+  
+  const navigate = useNavigate();
 
-  console.log(cookies)
   const viewLogin = (status) => {
     setError(null)
     setIsLogin(status)
   }
   console.log(email, password, confirmPassword);
+  
   const handleSubmit = async (e, endPoint) => {
     e.preventDefault()
-
-    const data = {email,password}
-
-    if (!isLogin && password !== confirmPassword) {
-      setError('Password does not match')
-      return
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
     }
-    //fetch API for SignUp And Login
-    const response = await fetch(`${process.env.REACT_APP_SERVERURL}/${endPoint}`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    })
-    //setCookie used in response of SighUo
-    const json = await response.json()
-    if(response.status === 200) {
-      console.log('Success')
-      setCookie('Email', json.email)
-      setCookie('AuthToken', json.token)
-      window.location.reload()
+    if (!password) {
+      setError("Please enter the password");
+      return;
     }
-    else {
-      setError('Something went wrong')
+    try {
+      const response = await axiosInstance.post(`${endPoint}`, {
+        email, 
+        password
+      })
+      console.log('response', response)
+      if (response.data && response.data.accessToken) {
+        console.log('response.data found',response.data)
+        console.log('response.data.token found',response.data.accessToken)
+        localStorage.setItem("token", response.data.accessToken)
+        navigate('/dashboard')
+        getData()
+      }
+    } 
+    catch (error) {
+      if (error.response && error.response.data && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error occurred. Please try again.");
+      }
     }
   }
-
   return (
     <div clasName="auth-container">
       <div clasName="auth-container">
         <form>
           <h2>{isLogin ? 'Please LogIn' : 'Please SignUp'}</h2>
-          <input type="email" placeholder="email" onChange={(e) => setEmail(e.target.value)}></input>
+          <input 
+          type="email" 
+          placeholder="email" 
+          onChange={(e) => setEmail(e.target.value)}></input>
           <input type="passsword" placeholder="password" onChange={(e) => setPassword(e.target.value)}></input>
           {!isLogin && <input type="password" placeholder="confirm password" onChange={(e) => setConfirmPassword(e.target.value)}></input>}
-          <input type="submit" className="create" onClick={(e) => handleSubmit(e, isLogin ? 'login' : 'signup')}></input>
+          <input type="submit" className="create" onClick={(e) => handleSubmit(e, isLogin ? 'login' : 'create-account')}></input>
           {error && <p>{error}</p>}
         </form>
         <div className="auth-options">
