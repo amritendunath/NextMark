1988925c4036a21fa9d965c676a2d7032b501fb5
Amritendu Nath <amritendunath@yahoo.com>
2024-11-29 14:19:28 +0530

    moved getData func from App.js to Get.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,36 @@
-import ListHeader from "./Components/ListHeader";
-import { useState, useEffect } from 'react';
-import ListItem from "./Components/ListItem";
 import Auth from "./Components/Auth";
-import { useCookies } from "react-cookie";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import ListItem from "./Components/ListItem";
+import ListHeader from "./Components/ListHeader";
+import Home from "./Get";
+
+
 
 const App = () => {
 
-  const [cookies, setCookie, removeCookie] = useCookies(null);
-
-  const authToken = cookies.AuthToken
-  const userEmail = cookies.Email
-
-
-  const [tasks, setTasks] = useState([]);
-
-  const getData = async () => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`);
-      const json = await response.json();
-      console.log(json)
-      setTasks(json)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  useEffect(() => {
-    if (authToken) {
-      getData()
-    }
-  }, [])
-
-  console.log(tasks)
-
-  //sort by date
-  const sortedTasks = Array.isArray(tasks) ? tasks.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate)) : [];
-
-  // const sortedTasks = tasks?.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate))
-
   return (
     <div className="app">
-      {!authToken && <Auth />}
-      {authToken &&
-        <>
-          <ListHeader listName={"🏝️Holiday Tick List"} getData={getData} />
-          <p className="user-email">Welcome back {userEmail}</p>
-          {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
-
-        </>
-      }
-      <p className="copyright">© NextMark Software Lab</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/dashboard" exact element={<Home/>}/>
+          <Route path="/login" exact element={<Auth/>} />
+          <Route path="/signUp" exact element={<Auth/>} />
+        </Routes>
+      </Router>
     </div>
+
   );
 }
+const Root = () => {
+  // Check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+  // Redirect to dashboard if authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
 
 export default App;
