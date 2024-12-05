a8d7e8b512eeb8537f819d96dc96b856c1a1afa7
Amritendu Nath <amritendunath@yahoo.com>
2024-12-05 22:14:48 +0530

    lines

--- client/src/Components/ListHeader.js
+++ client/src/Components/ListHeader.js
@@ -10,7 +10,11 @@ const ListHeader = ({ listName, getData }) => {
     localStorage.clear()
     navigate("/login");
   }
-
+//   useEffect(() => {
+//     console.log('useEffect triggered')
+//     getData();
+//     return () => {};
+//   },[]);
   return (
     <div className="list-header">
       <h1>{listName}</h1>
